2981ce505d9797efe768daff2bea38b4fc76c007
rksorry7255tu <rk7255053708@gmail.com>
2024-08-15 12:33:37 +0530

    modification

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const path=require("path");
 const { clearScreenDown } = require("readline");
-require("./db/conn");
+require("../expressweb1/src/db/conn");
 const hbs =require("hbs");
 
 
